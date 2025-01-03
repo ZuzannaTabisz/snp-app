d86734e16610f43e9dc71023fbfb85216338cf46
Zuzanna Tabisz <zuzanna.tabisz@student.put.poznan.pl>
2025-01-03 14:49:47 +0100

    hero button change

--- frontend/src/components/Hero/index.tsx
+++ frontend/src/components/Hero/index.tsx
@@ -22,13 +22,13 @@ const Hero = () => {
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
-                    href="https://nextjstemplates.com/templates/saas-starter-startup"   //zmienić link
+                    href="/pair"
                     className="rounded-sm bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
                   >
                     Comparison
                   </Link>
                   <Link
-                    href="https://github.com/NextJSTemplates/startup-nextjs"    //zmienić link
+                    href="/single"
                     className="inline-block rounded-sm bg-black px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5"
                   >
                     Top 10 SNPs
